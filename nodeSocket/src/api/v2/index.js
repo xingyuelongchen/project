eba6653d29e1b8bb15f09468623809660f26d2d2
xingyuelongchen <xingyuelongchen@qq.com>
2020-09-15 18:02:43 +0800

    update

--- nodeSocket/src/api/v2/index.js
+++ nodeSocket/src/api/v2/index.js
@@ -1,4 +1,3 @@
-var pingpp = require('pingpp-js');
 module.exports = function (req, res, config) {
     try {
         ({ 'get': get, 'post': post, 'delete': del, 'put': put }[req.method.toLowerCase()])(req, res, config)
@@ -7,22 +6,8 @@ module.exports = function (req, res, config) {
     }
 }
 function get(req, res, config) {
-    pingpp.createPayment(object, function (result, err) {
-        // object 需是 Charge/Order/Recharge 的 JSON 字符串
-        // 可按需使用 alert 方法弹出 log
-        //console.log(JSON.stringify(object));
-        console.log("result:" + result);
-        console.log("err.msg:" + err.msg);
-        console.log("err.extra:" + err.extra);
-        if (result == "success") {
-            // 只有微信JSAPI (wx_pub)、微信小程序（wx_lite）、QQ 公众号 (qpay_pub)、支付宝小程序（alipay_lite）支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL
-        } else if (result == "fail") {
-            // Ping++ 对象 object 不正确或者微信JSAPI/微信小程序/QQ公众号支付失败时会在此处返回
-        } else if (result == "cancel") {
-            // 微信JSAPI、微信小程序、QQ 公众号、支付宝小程序支付取消支付
-        }
-    });
-    // res.send('v2-index - get')
+    
+    res.send('v2-index - get')
 }
 function post(req, res, config) {
     res.send('v2-index - post')
--- src/views/backend/web/index.vue
+++ src/views/backend/web/index.vue
@@ -4,13 +4,40 @@ Create author: qinglong
 Create Time  : 2020-07-24
 -->
 <template>
-  <div class="content">功能筹备中</div>
+  <div class="content">功能筹备中
+    <el-button @click="getPay">
+
+    </el-button>
+    <img :src="src" alt="" srcset="">
+  </div>
 </template>
 <script>
+import pingpp from "pingpp-js";
 export default {
   name: "Extendweb",
   data() {
-    return {};
+    return { src: null };
+  },
+  methods: {
+    async getPay() {
+      console.log(0);
+      let { data } = await this.axios("/adminapi/Analysis/list");
+      pingpp.createPayment(data.data, function(result, err) {
+        // object 需是 Charge/Order/Recharge 的 JSON 字符串
+        // 可按需使用 alert 方法弹出 log
+        // console.log(JSON.stringify(data));
+        console.log("result:" + result);
+        console.log("err.msg:" + err.msg);
+        console.log("err.extra:" + err.extra);
+        if (result == "success") {
+          // 只有微信JSAPI (wx_pub)、微信小程序（wx_lite）、QQ 公众号 (qpay_pub)、支付宝小程序（alipay_lite）支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL
+        } else if (result == "fail") {
+          // Ping++ 对象 object 不正确或者微信JSAPI/微信小程序/QQ公众号支付失败时会在此处返回
+        } else if (result == "cancel") {
+          // 微信JSAPI、微信小程序、QQ 公众号、支付宝小程序支付取消支付
+        }
+      });
+    }
   }
 };
 </script>
